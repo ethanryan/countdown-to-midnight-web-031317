3b2e5f2907df71cfb492b7a46defcc82ee43ce1d
Ethan Ryan <ethanryan@gmail.com>
2017-02-28 04:32:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
-end
+def countdown(number_of_times)
+  x = 10
+  until x < 1
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end #end until loop
+  return "HAPPY NEW YEAR!"
+end #end method
+
+def countdown_with_sleep(number_of_times)
+  x = 10
+  until x < 1
+    sleep 5 #sleep 5 seconds before each output... this goes inside the loop
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end #end until loop
+  return "HAPPY NEW YEAR!"
+end #end method
